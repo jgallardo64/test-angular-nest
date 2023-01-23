8234a1dbba8fbe4135d993cc4997deeacb441afd
Jorge Gallardo <jorge.gallardo-garcia@capgemini.com>
2023-01-23 20:12:56 +0100

    Initial commit

---
+++ apps/formula-payo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'formula-payo-web-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'formula-payo';
+}
